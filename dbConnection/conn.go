4022417248bcc2d8a44438d9eda9b37a5cd764cd
hamkasatria <muhhamkasatrianto@gmail.com>
2021-10-14 19:27:15 +0700

    add databse

---
+++ dbConnection/conn.go
@@ -0,0 +1,84 @@
+package main
+
+/*
+package digunakan untuk mengakses database postgres
+host :
+port :
+
+*/
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func main() {
+	//mengakses DB
+
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "latihanGo",
+	}
+
+	//Get Database handle
+	var err error
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Print("CONNECTED")
+
+	//memangil datanya
+	albums, err := albumByArtist("Jhon Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found : %v", albums)
+}
+
+func albumByArtist(name string) ([]Album, error) {
+
+	var albums []Album
+
+	fmt.Print("--DI--")
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	fmt.Print("--D2--")
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
